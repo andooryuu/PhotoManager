3aebe480810f419503cc7df3c2eab1c7c0b8cfc9
Andrew Kartsakis <andrew.kartsakis@outlook.com>
2023-12-03 13:41:10 -0500

    f d
    vfbfrrbe

--- response.js
+++ response.js
@@ -91,7 +91,7 @@ export default class Response {
     unverifiedUser(errormessage = '') { return this.status(480, errormessage); }  // custom bad request status
     userNotFound(errormessage = '') { return this.status(481, errormessage); }    // custom bad request status
     wrongPassword(errormessage = '') { return this.status(482, errormessage); }   // custom bad request status
-
+    alreadyAdmin(errormessage =''){return this.status(483,errormessage);}         // custom bad request status
     /////////////////////////////////////////////// 500 ///////////////////////////////////////////////////////
 
     internalError(errormessage = '') { return this.status(500, errormessage); }   // internal error status
